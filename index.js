5396cc872324746f30a37a67a3d6545892b8ab84
AdrianSzlag <92638498+AdrianSzlag@users.noreply.github.com>
2022-12-16 22:58:21 +0100

    api

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+const { v4: uuidv4 } = require('uuid');
 
 //app.use(express.static(__dirname)); // Current directory is root
 app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+
+var meeting_list=new Array();
+
+
+app.post("/make", (req, res, next) => {
+    let meeting = {
+        uuid: uuidv4(),
+        hmm: "hmmm"
+    }
+    res.json(meeting);
+});
+
 
 app.listen(80);
 console.log('Listening on port 80');
