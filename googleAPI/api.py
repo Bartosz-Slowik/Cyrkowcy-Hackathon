8177b0ecf7e385b06cf20ff3b89f6a1946719c4b
Bartosz <bartosz.slowik@student.pk.edu.pl>
2022-12-17 01:33:36 +0100

    version alpha

--- googleAPI/api.py
+++ googleAPI/api.py
@@ -3,8 +3,13 @@ from gcsa.google_calendar import GoogleCalendar
 from gcsa.recurrence import Recurrence, DAILY, SU, SA
 
 from beautiful_date import Jan, Apr
-gc = GoogleCalendar(credentials_path='googleAPI\.credentials\credentials.json')
 
+calendars=[]
+for i in range(0,3):
+    path = 'googleAPI/.credentials/user'+str(i)+'_token.pickle'
+    gc = GoogleCalendar(credentials_path='googleAPI\.credentials\credentials.json',
+                    token_path=path)
+    calendars.append(gc)
 #calendar = GoogleCalendar('ritit.server@gmail.com')
 
 class MyEvent:
@@ -31,11 +36,35 @@ event = Event(
 
 #calendar.add_event(event)
 
-for event in gc:
-    print(event.start)
-    print(event.end)
-    print(event.summary)
-    #print(event.description)
-    #print(event.location)
-    print(event.attendees)
-    #print(event.minutes_before_email_reminder)
+class MyEvent:
+    def __init__(self, name, desc, attendees, length):
+        self.name = name
+        self.desc = desc
+        self.attendees = attendees
+        self.length = length
+
+events = {}
+for gc in calendars:
+    for event in gc:
+        temp=[]
+        if event.attendees==[]:
+            temp.append(event.creator.email)
+        else:
+            for attendee in event.attendees:
+                temp.append(attendee.email)
+        events[event.event_id] = MyEvent(event.summary, event.description, temp, event.end-event.start)
+
+
+print(events)
+'''
+for gc in calendars:
+    for event in gc:
+        print(event.attendees)
+        print(event.creator.email)
+        print(event.event_id)
+        print(event.start)
+        print(event.end)
+
+'''
+
+
